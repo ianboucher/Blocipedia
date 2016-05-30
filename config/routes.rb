5ac6e5a8fbe43af578dbaabd9db338ebac93f148
Ian Boucher <i.a.boucher@gmail.com>
2016-05-30 10:41:24 +0100

    Minor correction to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   resources :memberships, only: [:new, :create, :destroy]
 
-  resources :wikis 
+  resources :wikis
 
   authenticated :user do
     root 'wikis#index', as: :authenticated_root
